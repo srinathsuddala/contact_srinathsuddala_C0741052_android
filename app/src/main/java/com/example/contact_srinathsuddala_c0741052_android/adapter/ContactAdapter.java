da935e8a09646269f8516a15e69052e75aeae5fb
srinathsuddala <srinathsuddala@gmail.com>
2020-07-16 19:29:05 +0530

    added ContactAdapter.java

---
+++ app/src/main/java/com/example/contact_srinathsuddala_c0741052_android/adapter/ContactAdapter.java
@@ -0,0 +1,55 @@
+package com.example.contact_srinathsuddala_c0741052_android.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.contact_srinathsuddala_c0741052_android.R;
+import com.example.contact_srinathsuddala_c0741052_android.model.Contact;
+
+import java.util.ArrayList;
+
+public class ContactAdapter extends ArrayAdapter<Contact> {
+    ArrayList<Contact> contacts;
+    public ContactAdapter(@NonNull Context context, int resource, ArrayList<Contact> contacts) {
+        super(context, resource);
+        this.contacts = contacts;
+    }
+
+    @Override
+    public int getCount() {
+        return contacts.size();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = convertView;
+        if(view == null){
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            view = inflater.inflate(R.layout.contact_list_items,null);
+        }
+
+        Contact contact = contacts.get(position);
+
+        if(contact != null){
+            TextView textTitle = (TextView) view.findViewById(R.id.textName);
+            TextView textMobile = (TextView) view.findViewById(R.id.textNumber);
+
+            if(textTitle != null)
+                textTitle.setText(contact.getFirstName());
+            if(textMobile != null)
+                textMobile.setText(contact.getMobile());
+        }
+
+
+        return view;
+    }
+}
