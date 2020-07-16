03dab51557e55fc207026d4a10ae491ce9c3f996
srinathsuddala <srinathsuddala@gmail.com>
2020-07-16 19:34:11 +0530

    added ContactClickListner.java

---
+++ app/src/main/java/com/example/contact_srinathsuddala_c0741052_android/common/ContactClickListner.java
@@ -0,0 +1,8 @@
+package com.example.contact_srinathsuddala_c0741052_android.common;
+
+
+import com.example.contact_srinathsuddala_c0741052_android.model.Contact;
+
+public interface ContactClickListner {
+    void onContactClick(Contact contact);
+}
